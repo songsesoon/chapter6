6b73207c7fff532646bf5141458790e7a7f96cb0
세순 송 <sss010201sss@naver.com>
2023-03-31 16:37:14 +0900

    초기 커밋.

---
+++ songsesoon_ex1/MainApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace songsesoon_ex1
+{
+    class MainApp
+    {
+        static double Square(double arg)
+        {
+            double result;
+            result = arg * arg;
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("수를 입력하세요: ");
+            string input = Console.ReadLine();
+            double arg = Convert.ToDouble(input);
+
+            Console.WriteLine("결과: {0}", Square(arg));
+        }
+    }
+}
