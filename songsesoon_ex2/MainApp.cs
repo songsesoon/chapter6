9eee9dac91e6b52f2159c36f49e23abf857a5dcb
세순 송 <sss010201sss@naver.com>
2023-03-31 16:57:11 +0900

    2023.03.31.4.50

---
+++ songsesoon_ex2/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace songsesoon_ex2
+{
+    class MainApp
+    {
+        public static void Main()
+        {
+            double mean = 0;
+
+            mean = Mean(1, 2, 3, 4, 5, mean);
+            Console.WriteLine("평균 : {0}", mean);
+        }
+        public static double Mean(
+            double a, double b, double c,
+            double d, double e, double mean)
+        {
+            mean = (a + b + c + d + e) / 5;
+
+            return mean;
+        }
+    }
+}
